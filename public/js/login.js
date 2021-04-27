20e85676acd3088bbb6da2076bd8177cf4a7156e
Eloy Gonzalez <eloy522752@gmail.com>
2021-04-27 01:03:13 -0400

    add changes to layout

--- public/js/login.js
+++ public/js/login.js
@@ -12,6 +12,7 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
+      alert("it worked loggedin")
       document.location.replace('/');
     } else {
       alert('Failed to log in.');
